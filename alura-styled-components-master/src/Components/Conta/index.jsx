4474e7529ab35c5dbcb1b5fec11f6a0d37d75f31
Gabriel Garcia <gabrielgarciaofc@gmail.com>
2022-05-05 19:07:53 -0300

    Implementação extrato

---
+++ alura-styled-components-master/src/Components/Conta/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import privado from "../../assets/images/privado.svg";
+import olho_icone from "../../assets/images/olho.svg";
+import dinheiro from "../../assets/images/dinheiro.svg";
+import {Icone, Box, Button, Saldo, Detalhe} from "../UI/index";
+
+
+const IconeMargin = styled(Icone)`
+  margin-top: 2px;
+`; // Herda os estilos do component Icone  que está em ../UI/index.js
+
+
+const Conta = () => {
+  const [toggleState, untoggle] = useState(true);
+
+  const toggleHandler = () => {
+    untoggle((toggleState) => !toggleState);
+  };
+
+  return (
+    <Box>
+      <h2>Conta</h2>
+      <div style={{ fontSize: "26px", padding: "20px 0" }}>
+        Saldo disponível{" "}
+        <span>
+          <Icone src={dinheiro} alt="Ícone Saldo" />
+        </span>
+        {toggleState ? (
+          <Saldo>
+            <Detalhe>R$</Detalhe> 0,00{" "}
+          </Saldo>
+        ) : null}
+      </div>
+
+      <Button onClick={toggleHandler}>
+        <IconeMargin
+          src={toggleState ? privado : olho_icone}
+          alt="Privacidade do Saldo"
+        />
+      </Button>
+    </Box>
+  );
+};
+
+export default Conta;
